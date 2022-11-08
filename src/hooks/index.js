63302ca72ea134cde66ffd4cf3a1098164501ad3
onesine <onesine@dmdconsul.com>
2022-11-08 10:26:39 +0100

    Update code

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -14,27 +14,24 @@ export function useFetchData (endpoint) {
             .catch(({response}) => console.error("Something is wrong"))
             .finally(() => {setLoading(false)})
         ;
-    }, []);
+    }, [endpoint]);
 
     return [data, loading];
 }
 
 export function useOnClickOutside(ref, handler) {
-    useEffect(
-        () => {
-            const listener = (event) => {
-                if (!ref.current || ref.current.contains(event.target)) {
-                    return;
-                }
-                handler(event);
-            };
-            document.addEventListener("mousedown", listener);
-            document.addEventListener("touchstart", listener);
-            return () => {
-                document.removeEventListener("mousedown", listener);
-                document.removeEventListener("touchstart", listener);
-            };
-        },
-        [ref, handler]
-    );
+    useEffect(() => {
+        const listener = (event) => {
+            if (!ref.current || ref.current.contains(event.target)) {
+                return;
+            }
+            handler(event);
+        };
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+        return () => {
+            document.removeEventListener("mousedown", listener);
+            document.removeEventListener("touchstart", listener);
+        };
+    }, [ref, handler]);
 }
